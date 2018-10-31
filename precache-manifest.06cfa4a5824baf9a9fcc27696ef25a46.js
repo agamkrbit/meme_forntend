c598885e0064ec4a85672e1d59216a1b070b6b12
agamkrbit <agamkrbit123@gmail.com>
2018-10-31 13:37:09 +0530

    initital commit

---
+++ precache-manifest.06cfa4a5824baf9a9fcc27696ef25a46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7262af1dd51f1c0b420",
+    "url": "/static/js/main.d7262af1.chunk.js"
+  },
+  {
+    "revision": "67ad82fa65d9cca911a6",
+    "url": "/static/js/1.67ad82fa.chunk.js"
+  },
+  {
+    "revision": "d7262af1dd51f1c0b420",
+    "url": "/static/css/main.512e5254.chunk.css"
+  },
+  {
+    "revision": "636ee58de86a770ac9b548f02d65cabc",
+    "url": "/index.html"
+  }
+];
